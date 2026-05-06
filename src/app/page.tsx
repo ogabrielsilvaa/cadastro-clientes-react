0f973094a7839ffe7d0c24e14b9e324e460ee20a
ogabrielsilvaa <ogabrieusiuva@gmail.com>
2026-05-06 11:23:37 -0300

    chore: setup do projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import FormularioCliente from "../components/FormularioCliente";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-slate-100 px-4 py-10">
+      <section className="mx-auto max-w-4xl">
+        <div className="mb-8 text-center">
+          <h1 className="text-3xl font-bold text-slate-900">
+            Cadastro de Clintes
+          </h1>
+          <p className="mt-2 text-slate-600">
+            Projeto introdutório com React, Next.js e Tailwind CSS.
+          </p>
+        </div>
+
+        <FormularioCliente />
+      </section>
+    </main>
+  )
+}
---
+++ src/types/clientForm.ts
@@ -0,0 +1,8 @@
+export type ClienteForm = {
+  nome: string;
+  email: string;
+  senha: string;
+  telefone: string;
+  dataNascimento: string;
+  cpf: string;
+};
